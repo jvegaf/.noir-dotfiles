a91ed44f45826574c21abdd2b507ea2d4ce13943
Soma Noir <45913147+somanoir@users.noreply.github.com>
2025-05-19 00:33:03 +0300

    Nvim: add lualine in a separate file and adjust its theme for neopywal

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  enabled = true,
+  config = function()
+    local has_lualine, lualine = pcall(require, "lualine")
+    if not has_lualine then
+      return
+    end
+
+    local has_neopywal, neopywal_lualine = pcall(require, "neopywal.theme.plugins.lualine")
+    if not has_neopywal then
+      return
+    end
+
+    neopywal_lualine.setup()
+  end,
+}
